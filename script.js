d8c18dc40e827f84fefbe7e80e175a1dafbb8eae
batoollaiba172-spec <batoollaiba172@gmail.com>
2026-04-09 10:25:25 +0500

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,28 @@ function resetGame() {
     document.getElementById("message").textContent = "";
     document.getElementById("tries").textContent = "";
 }
+
+
+
+
+
+
+
+function updateClock() {
+  const now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+
+  hours = hours.toString().padStart(2, '0');
+  minutes = minutes.toString().padStart(2, '0');
+  seconds = seconds.toString().padStart(2, '0');
+
+  document.getElementById("clock").textContent =
+    `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
+
+
